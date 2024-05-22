8919f28c96f07659e95e84150d6ea5570b108c13
quanchao <virisred@gamil.com>
2024-05-22 14:23:19 +0800

    更新

--- os/taskx/cron.go
+++ os/taskx/cron.go
@@ -2,56 +2,55 @@ package taskx
 
 import (
 	"fmt"
+	"github.com/robfig/cron/v3"
 	"log"
 	"os"
 	"sync"
-	"time"
-
-	"github.com/robfig/cron/v3"
 )
 
 // 定时任务调度器
 type CronScheduler struct {
+	mutex *sync.Mutex
 	cron  *cron.Cron
-	mutex sync.Mutex
 	tasks map[string]*CronTask
 }
 
 // 定时任务
 type CronTask struct {
-	Id   string
-	Spec string
-	Next time.Time
+	name string
+	spec string
 	*cron.Entry
 }
 
 // 定时任务调度器
-func NewCronScheduler() *CronScheduler {
+func Corn() *CronScheduler {
 	//初始化一个定时任务调度程序
 	return &CronScheduler{
+		mutex: new(sync.Mutex),
 		cron: cron.New(
 			cron.WithSeconds(),
 			cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)),
 			cron.WithLogger(cron.VerbosePrintfLogger(log.New(os.Stdout, "cron: ", log.LstdFlags))),
 		),
-		mutex: sync.Mutex{},
 		tasks: make(map[string]*CronTask),
 	}
 }
 
+// 开始执行定时任务
 func (s *CronScheduler) Start() {
 	s.cron.Start()
 }
 
+// 停止执行定时任务
 func (s *CronScheduler) Stop() {
 	s.cron.Stop()
 }
 
-func (s *CronScheduler) AddTask(id string, spec string, task func()) error {
+func (s *CronScheduler) Add(name string, spec string, task func()) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 	// 定时任务如果已存在则先移除再新增
-	if entry, ok := s.tasks[id]; ok {
+	if entry, ok := s.tasks[name]; ok {
 		s.cron.Remove(entry.ID)
 	}
 	// 新增定时任务
@@ -59,12 +58,12 @@ func (s *CronScheduler) AddTask(id string, spec string, task func()) error {
 		return err
 	} else {
 		entry := s.cron.Entry(entryID)
-		s.tasks[id] = &CronTask{Id: id, Spec: spec, Entry: &entry}
+		s.tasks[name] = &CronTask{name: name, spec: spec, Entry: &entry}
 	}
 	return nil
 }
 
-func (s *CronScheduler) Tasks() []*CronTask {
+func (s *CronScheduler) All() []*CronTask {
 	var tasks []*CronTask
 	for _, task := range s.tasks {
 		tasks = append(tasks, task)
@@ -72,23 +71,23 @@ func (s *CronScheduler) Tasks() []*CronTask {
 	return tasks
 }
 
-func (s *CronScheduler) GetTask(id string) *CronTask {
+func (s *CronScheduler) Get(name string) *CronTask {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-	if task, ok := s.tasks[id]; ok {
+	if task, ok := s.tasks[name]; ok {
 		return task
 	}
 	return nil
 }
 
-func (s *CronScheduler) RemoveTask(id string) {
+func (s *CronScheduler) Remove(name string) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-	if entry, ok := s.tasks[id]; ok {
+	if entry, ok := s.tasks[name]; ok {
 		s.cron.Remove(entry.ID)
 	}
 }
 
 func (s *CronTask) Info() string {
-	return fmt.Sprintf("id:%s spec:%s next:%s", s.Id, s.Spec, s.Next.Format("2006-01-02 15:04:05"))
+	return fmt.Sprintf("name:%s spec:%s next:%s", s.name, s.spec, s.Next.Format("2006-01-02 15:04:05"))
 }
--- os/taskx/queue.go
+++ os/taskx/queue.go
@@ -5,109 +5,173 @@ import (
 )
 
 // 任务处理调度器
-type Queue struct {
+type QueueScheduler struct {
 	mutex *sync.Mutex
-	head  *QueueTask            // 首个任务
-	tail  *QueueTask            // 末尾任务
-	tasks map[string]*QueueTask // 所有任务
+	Head  *QueueTask            // 首个任务
+	Tail  *QueueTask            // 末尾任务
+	Tasks map[string]*QueueTask // 所有任务
 }
 
+// 对列任务
 type QueueTask struct {
-	name string
-	exec func()     // 当前任务
-	next *QueueTask // 指向下一个任务
+	name string       // 任务名
+	fn   func() error // 当前任务
+	prev *QueueTask   // 指向上·一个任务
+	next *QueueTask   // 指向下一个任务
 }
 
 func (t *QueueTask) HasNext() bool {
 	return t.next != nil
 }
 
-func NewQueue() *Queue {
-	return &Queue{
+func (t *QueueTask) Name() string {
+	return t.name
+}
+
+func Queue() *QueueScheduler {
+	return &QueueScheduler{
 		mutex: new(sync.Mutex),
-		tasks: make(map[string]*QueueTask),
+		Tasks: make(map[string]*QueueTask),
 	}
 }
 
-func (q *Queue) Execute() {
+// 执行
+func (q *QueueScheduler) Execute() (err error) {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
-	if curr := q.head; curr != nil {
-		curr.exec()
-		delete(q.tasks, curr.name)
-		for curr.HasNext() {
-			curr = curr.next
-			curr.exec()
-			delete(q.tasks, curr.name)
+	for q.Head != nil {
+		if err = q.Head.fn(); err != nil {
+			break
 		}
-		q.head = nil
-		q.tail = nil
+		delete(q.Tasks, q.Head.name)
+		q.Head = q.Head.next
 	}
+	if len(q.Tasks) == 0 {
+		q.Head = nil
+		q.Tail = nil
+	}
+	return
 }
 
-// 头插（当前新增任务添加到队列任务最后）
-func (q *Queue) Add(name string, fn func()) {
-	q.AddTail(name, fn)
+// 新增（默认尾插）
+func (q *QueueScheduler) Add(name string, fn func() error) {
+	if name != "" && fn != nil {
+		q.AddTail(name, fn)
+	}
 }
 
 // 尾插（当前新增任务添加到队列末尾）
-func (q *Queue) AddTail(name string, fn func()) {
-	if name == "" || fn == nil {
-		return
-	}
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
-	if _, ok := q.tasks[name]; !ok {
-		var newTask = &QueueTask{name: name, exec: fn}
-		if tail := q.tail; tail != nil {
-			tail.next = newTask
-			q.tail = newTask
-		} else {
-			q.head = newTask
-			q.tail = newTask
+func (q *QueueScheduler) AddTail(name string, fn func() error) {
+	if name != "" && fn != nil {
+		q.mutex.Lock()
+		defer q.mutex.Unlock()
+		if _, exist := q.Tasks[name]; !exist {
+			var add = &QueueTask{name: name, fn: fn}
+			if tail := q.Tail; tail != nil {
+				add.prev = tail
+				tail.next = add
+				q.Tail = add
+			} else {
+				q.Head = add
+				q.Tail = add
+			}
+			q.Tasks[name] = add
 		}
-		q.tasks[name] = newTask
 	}
 }
 
 // 头插（当前新增任务添加到队列首位）
-func (q *Queue) AddHead(name string, fn func()) {
-	if name == "" || fn == nil {
-		return
-	}
-	q.mutex.Lock()
-	defer q.mutex.Unlock()
-	if _, ok := q.tasks[name]; !ok {
-		var newTask = &QueueTask{name: name, exec: fn}
-		if head := q.head; head != nil {
-			newTask.next = head
-			q.head = newTask
-		} else {
-			q.head = newTask
-			q.tail = newTask
+func (q *QueueScheduler) AddHead(name string, task func() error) {
+	if name != "" && task != nil {
+		q.mutex.Lock()
+		defer q.mutex.Unlock()
+		if _, exist := q.Tasks[name]; !exist {
+			var add = &QueueTask{name: name, fn: task}
+			if head := q.Head; head != nil {
+				head.prev = add
+				add.next = head
+				q.Head = add
+			} else {
+				q.Head = add
+				q.Tail = add
+			}
+			q.Tasks[name] = add
 		}
-		q.tasks[name] = newTask
 	}
 }
 
-// 插队注册任务(当前新增任务添加到after任务之后)
-func (q *Queue) AddJump(name string, fn func(), after string) {
-	if name == "" || fn == nil {
-		return
+// 后插队(当前新增任务添加到after任务之后)
+func (q *QueueScheduler) AddAfter(name string, task func() error, after string) {
+	if name != "" && task != nil {
+		q.mutex.Lock()
+		defer q.mutex.Unlock()
+		if _, exist := q.Tasks[name]; !exist {
+			var add = &QueueTask{name: name, fn: task}
+			if target, ok := q.Tasks[after]; ok && target.next != nil {
+				add.next = target.next
+				add.prev = target
+				target.next.prev = add
+				target.next = add
+			} else {
+				target = q.Tail
+				target.next = add
+				add.prev = target
+				q.Tail = add
+			}
+			q.Tasks[name] = add
+		}
 	}
+}
+
+// 前插队(当前新增任务添加到before任务之后)
+func (q *QueueScheduler) AddBefore(name string, task func() error, before string) {
+	if name != "" && task != nil {
+		q.mutex.Lock()
+		defer q.mutex.Unlock()
+		if _, exist := q.Tasks[name]; !exist {
+			var add = &QueueTask{name: name, fn: task}
+			if target, ok := q.Tasks[before]; ok && target.prev != nil {
+				add.prev = target.prev
+				add.next = target
+				target.prev.next = add
+				target.prev = add
+			} else {
+				target = q.Head
+				target.prev = add
+				add.next = target
+				q.Head = add
+			}
+			q.Tasks[name] = add
+		}
+	}
+}
+
+// 移除任务
+func (q *QueueScheduler) Remove(name string) {
+	if name != "" {
+		q.mutex.Lock()
+		defer q.mutex.Unlock()
+		if task, ok := q.Tasks[name]; ok {
+			if task.prev == nil {
+				task.next.prev = nil
+				q.Head = task.next
+			} else if task.next == nil {
+				task.prev.next = nil
+				q.Tail = task.prev
+			} else {
+				task.prev.next = task.next
+				task.next.prev = task.prev
+			}
+			delete(q.Tasks, name)
+		}
+	}
+}
+
+// 清除所有任务
+func (q *QueueScheduler) Clear() {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
-	if _, ok := q.tasks[name]; !ok {
-		var newTask = &QueueTask{name: name, exec: fn}
-		var jump *QueueTask
-		if jump, ok = q.tasks[after]; ok && jump.next != nil {
-			newTask.next = jump.next
-			jump.next = newTask
-		} else {
-			jump = q.tail
-			jump.next = newTask
-			q.tail = newTask
-		}
-		q.tasks[name] = newTask
-	}
+	q.Head = nil
+	q.Tail = nil
+	q.Tasks = make(map[string]*QueueTask)
 }
