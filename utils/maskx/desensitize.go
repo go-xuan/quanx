fe48e7f3adef04ce3f8bea4967e22f8e149bfd00
quanchao <virisred@gamil.com>
2024-06-04 23:11:51 +0800

    更新

---
+++ utils/maskx/desensitize.go
@@ -0,0 +1,35 @@
+package maskx
+
+import "strings"
+
+type Desensitize uint
+
+const mask = "*"
+const (
+	Phone Desensitize = iota
+	Name
+	IdCard
+	Email
+	Default // 全打码
+)
+
+func (d Desensitize) Desensitize(s string) string {
+	if s != "" {
+		l := len(s)
+		switch {
+		case d == Phone && l == 11:
+			s = s[:3] + strings.Repeat(mask, 4) + s[7:]
+		case d == IdCard && l == 18:
+			s = s[:4] + strings.Repeat(mask, 11) + s[15:]
+		case d == Name:
+			split := strings.Split(s, "")
+			s = split[0] + strings.Repeat(mask, len(split)-1)
+		case d == Email:
+			i := strings.Index(s, "@")
+			s = strings.Repeat(mask, i) + s[i:]
+		default:
+			s = strings.Repeat(mask, l)
+		}
+	}
+	return s
+}
