f46293d28c8e4165d1c9434d88d713f51c331cd9
quanchao <quanchao@jwzg.com>
2024-12-13 18:24:44 +0800

    开发进度保存

--- os/execx/exec.go
+++ os/execx/exec.go
@@ -9,7 +9,7 @@ import (
 )
 
 // ExecCommand 执行命令
-func ExecCommand(command string, in ...io.Reader) (string, error) {
+func ExecCommand(command string, in ...io.Reader) (string, string, error) {
 	if runtime.GOOS == `windows` {
 		return execCommandOnWindows(command, in...)
 	} else {
@@ -17,7 +17,7 @@ func ExecCommand(command string, in ...io.Reader) (string, error) {
 	}
 }
 
-func execCommandOnLinux(command string, in ...io.Reader) (string, error) {
+func execCommandOnLinux(command string, in ...io.Reader) (string, string, error) {
 	cmd := exec.Command("/bin/bash", `-c`, command)
 	cmd.Dir = "./"
 	if len(in) > 0 {
@@ -26,7 +26,7 @@ func execCommandOnLinux(command string, in ...io.Reader) (string, error) {
 	return commandRun(cmd)
 }
 
-func execCommandOnWindows(command string, in ...io.Reader) (string, error) {
+func execCommandOnWindows(command string, in ...io.Reader) (string, string, error) {
 	cmd := exec.Command("cmd", `/C`, command)
 	if len(in) > 0 {
 		cmd.Stdin = ioutil.NopCloser(in[0])
@@ -34,14 +34,14 @@ func execCommandOnWindows(command string, in ...io.Reader) (string, error) {
 	return commandRun(cmd)
 }
 
-func commandRun(cmd *exec.Cmd) (string, error) {
+func commandRun(cmd *exec.Cmd) (string, string, error) {
 	// 设置接收
 	var stdout, stderr = &bytes.Buffer{}, &bytes.Buffer{}
 	cmd.Stdout, cmd.Stderr = stdout, stderr
 	// 执行命令
 	if err := cmd.Run(); err != nil {
-		return "\n" + stdout.String() + "\n" + stderr.String(), err
+		return stdout.String(), stderr.String(), err
 	} else {
-		return "\n" + stdout.String(), nil
+		return stdout.String(), stderr.String(), nil
 	}
 }
